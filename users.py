cfeea8a82a9ac5427faae4d28fca6fa05b1349bd
Cole Hostasa <chostasa@sgghlaw.com>
2025-07-14 10:56:00 -0500

    ✅username and password

--- users.py
+++ users.py
@@ -7,7 +7,7 @@ def hash_password(password):
 
 USERS = {
     "chostasa@sgghlaw.com": hash_password("S!282600881348uh!"),
+    "toliver@sgghlaw.com": hash_password("G)177471332680ux"),
+    "qyu@sgghlaw.com": hash_password("5211384XMqo!"),
     "kasia": hash_password("Leadership22"),
-    "sydney": hash_password("AdminView3"),
-    "jordan": hash_password("CaseOps!4"),
 }
